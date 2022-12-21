3f4f153d9ff5e59b00265274a4f5deba5a197573
刘镕硕 <99574908+lrsgzs@users.noreply.github.com>
2022-12-21 10:14:12 +0800

    upload peoples

--- index.js
+++ index.js
@@ -30,14 +30,32 @@ peoples = {
     }
 }
 
-var doc = "";
-for (var i in peoples) {
-    doc = doc + "<br>"
-    doc = doc + "<h5>" + i + "</h5>";
-    for (var j in peoples[i]) {
-        doc = doc + '<p><a href="' + peoples[i][j] + '">' + j + "</a></p> ";
-    }
+// 解码
+function getDecode(str){
+    return decodeURIComponent(atob(str).split('').map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
 }
 
-var tag = document.getElementById("peoples");
-tag.innerHTML = tag.innerHTML + doc;
+// 显示人员
+var show_peoples = function(data){
+    peopels_str = getDecode(data["data"]["content"]).replaceAll("\r", "");  // 获取文章markdown（md）数据
+    
+    var script_dom = document.createElement('script');  // 创建标签
+    script_dom.language = 'javascript';  // 设置语言
+    script_dom.type = 'text/javascript';  // 设置类型
+    script_dom.innerHTML = peopels_str + "\n" + "var doc = '';for (var i in peoples) {doc = doc + '<br>';doc = doc + '<h5>' + i + '</h5>';for (var j in peoples[i]) {doc = doc + '<p><a href=\"' + peoples[i][j] + '\">' + j + '</a></p>';}}var tag = document.getElementById('peoples');tag.innerHTML = tag.innerHTML + doc;";  // 设置脚本
+    var head = document.getElementsByTagName('head').item(0);  //获取head标签
+    head.appendChild(script_dom);  //添加标签
+}
+
+// 构造api接口url
+var url = "https://api.github.com/repos/lrsgzs/blog_topics/contents/yls.js?callback=showpeople"
+
+// 通过callback和script标签来加载api，解决跨域问题
+var script_dom = document.createElement('script');  // 创建标签
+script_dom.src = url;  //设置src
+script_dom.language = 'javascript';  // 设置语言
+script_dom.type = 'text/javascript';  // 设置类型
+var head = document.getElementsByTagName('head').item(0);  //获取head标签
+head.appendChild(script_dom);  //添加标签
